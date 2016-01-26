0fe428dced8121f9cc1f9d803221082ccec5cd97
Dmitry <root@wilix.ru>
2016-01-26 20:03:03 +0300

    Update default deploy recipe

--- configure/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'configure'
-maintainer       'XEEPIC'
-maintainer_email 'lead@w.wilix.ru'
-license          'All rights reserved'
-description      'Configures'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- deploy/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'deploy'
-maintainer       'XEEPIC'
-maintainer_email 'lead@w.wilix.ru'
-license          'All rights reserved'
-description      'Deploy app'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
